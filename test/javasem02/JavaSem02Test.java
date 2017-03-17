cce63e7b1cafb06fb237460626349cb40fd7459c
Elīna Kuzmina <e.kuzmina11@gmail.com>
2017-03-17 11:00:32 +0200

    Created jUnit Test

---
+++ test/javasem02/JavaSem02Test.java
@@ -0,0 +1,186 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javasem02;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author BOX
+ */
+public class JavaSem02Test {
+    JavaSem02TeacherTest teacher = new JavaSem02TeacherTest();
+    public JavaSem02Test() {
+        //JavaSem02Test t = new JavaSem02Test();
+    }
+    
+
+    /**
+     * Test of main method, of class JavaSem02.
+     */
+    @Test
+    public void testMain() {
+        teacher.testMain();
+    }
+
+    /**
+     * Test of calculate method, of class JavaSem02.
+     */
+//    @Test
+//    public void testCalculate() {
+//        teacher.testC();
+//    }
+
+    /**
+     * Test of factorialForLoop method, of class JavaSem02.
+     */
+    @Test
+    public void testFactorialForLoop() {
+        teacher.testFactorialForLoop();
+    }
+
+    /**
+     * Test of factorialRecursive method, of class JavaSem02.
+     */
+    @Test
+    public void testFactorialRecursive() {
+        teacher.testFactorialRecursive();
+    }
+
+    /**
+     * Test of positionCalc method, of class JavaSem02.
+     */
+    @Test
+    public void testPositionCalc() {
+        teacher.testPositionCalc();
+    }
+
+    /**
+     * Test of printMarathonists method, of class JavaSem02.
+     */
+//    @Test
+//    public void testPrintMarathonists() {
+//        
+//    }
+
+    /**
+     * Test of createMarathonists method, of class JavaSem02.
+     */
+//    @Test
+//    public void testCreateMarathonists() {
+//        
+//    }
+
+    /**
+     * Test of generateArray method, of class JavaSem02.
+     */
+    @Test
+    public void testGenerateArray() {
+        teacher.testGenerateArray();
+    }
+
+    /**
+     * Test of getMean method, of class JavaSem02.
+     */
+    @Test
+    public void testGetMean() {
+        teacher.testGetMean();
+    }
+
+    /**
+     * Test of getMin method, of class JavaSem02.
+     */
+    @Test
+    public void testGetMin() {
+        teacher.testGetMin();
+    }
+
+    /**
+     * Test of getMax method, of class JavaSem02.
+     */
+    @Test
+    public void testGetMax() {
+        teacher.testGetMax();
+    }
+
+    /**
+     * Test of arraySort method, of class JavaSem02.
+     */
+    @Test
+    public void testArraySort() {
+        teacher.testArraySort();
+    }
+
+    /**
+     * Test of generateMatrix method, of class JavaSem02.
+     */
+    @Test
+    public void testGenerateMatrix() {
+        teacher.testGenerateMatrix();
+    }
+
+    /**
+     * Test of getProduct method, of class JavaSem02.
+     */
+    @Test
+    public void testGetProduct() {
+        teacher.testGetProduct();
+    }
+
+    /**
+     * Test of coinFlip method, of class JavaSem02.
+     */
+    @Test
+    public void testCoinFlip() {
+        teacher.testCoinFlip();
+    }
+
+    /**
+     * Test of rollDice method, of class JavaSem02.
+     */
+    @Test
+    public void testRollDice() {
+        teacher.testRollDice();
+    }
+
+    /**
+     * Test of roll2Dices method, of class JavaSem02.
+     */
+    @Test
+    public void testRoll2Dices() {
+        teacher.testRoll2Dices();
+    }
+
+    /**
+     * Test of getTextFromBytes method, of class JavaSem02.
+     */
+    @Test
+    public void testGetTextFromBytes() {
+        teacher.testGetTextFromBytes();
+    }
+
+    /**
+     * Test of pascalsTriangle method, of class JavaSem02.
+     */
+    @Test
+    public void testPascalsTriangle() {
+        teacher.testPascalsTriangle();
+    }
+
+    /**
+     * Test of executeStringEquation method, of class JavaSem02.
+     */
+    @Test
+    public void testExecuteStringEquation() {
+        teacher.testExecuteStringEquation();
+    }
+    
+}
